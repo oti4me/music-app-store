db4519599d23d2b9a2960084702c873f4ab35310
oti4me <oti4me@gmail.com>
2019-02-16 20:56:39 +0100

    add cross origin resource sharing middleware to allow request form other servers.

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\helpers\AuthHelpers;
+
+class CorsMiddleware
+{
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @return mixed
+   */
+  public function handle($request, Closure $next)
+  {
+    if ($request->isMethod('OPTIONS')) {
+      $response = Response::make();
+    } else {
+      $response = $next($request);
+    }
+
+    return $response
+      ->header('Access-Control-Allow-Origin', '*')
+      ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+      ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application, text')
+      ->header('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+  }
+}
